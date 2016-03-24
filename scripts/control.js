248af987b614a1ca2f9f4743484ef4039a5afe59
Derek Hancock <derekhancock44@gmail.com>
2016-03-24 00:15:07 -0600

    initial commit

---
+++ scripts/control.js
@@ -0,0 +1,12 @@
+//initialize switches
+$("[name='my-checkbox']").bootstrapSwitch();
+
+//click events
+$(".caret").add(".item-title").click(function() {
+        var parent = $(this).parent();
+        $(parent).find("span").toggleClass("right");
+        $(parent).find("span").toggleClass("down");
+        
+        var visibility = $(parent).find(".item-details").toggle();
+        
+})
