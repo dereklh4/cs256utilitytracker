b3492c97faa4e136405e0ed538f537b0efe47632
Derek Hancock <derekhancock44@gmail.com>
2016-03-29 23:42:11 -0600

    added total to control page

--- scripts/control.js
+++ scripts/control.js
@@ -7,16 +7,23 @@ $(document).ready(function() {
         var item_value = Number(( $(this).attr('id') ));
         var item_cost_element = ( $(this).parent().parent().parent().find(".item-cost"));
         var total_value = Number(item_cost_element.attr("id"));
+        var control_total_value = Number( $(".total-cost").attr("id"));
         
         if ($(this).hasClass("On")) {
             total_value += item_value;
+            control_total_value += item_value;
             item_cost_element.attr("id", total_value.toString());
             item_cost_element.html("-$" + total_value.toLocaleString());
+            $(".total-cost").attr("id", control_total_value.toPrecision(3));
+            $(".total-cost").html("Total: -$" + control_total_value.toPrecision(3) + " per hour");
         }
         else {
             total_value -= item_value;
+            control_total_value -= item_value;
             item_cost_element.attr("id", total_value.toString());
             item_cost_element.html("-$" + total_value.toLocaleString());
+            $(".total-cost").attr("id", control_total_value.toPrecision(3));
+            $(".total-cost").html("Total: -$" + control_total_value.toPrecision(3) + " per hour");
         }
         console.log(total_value);
     });
@@ -33,6 +40,7 @@ $(document).ready(function() {
     
     $("#arrow-up").on("click", function() {
         var temp_value = Number($(".temp-degree").attr("id"));
+        var control_total_value = Number( $(".total-cost").attr("id"));
         if (temp_value < 80) {
             //change temp display
             temp_value++;
@@ -44,11 +52,16 @@ $(document).ready(function() {
             cost += 0.24;
             $(".temp-item-cost").attr("id", cost.toLocaleString());
             $(".temp-item-cost").html("-$" + cost.toLocaleString());
+            
+            control_total_value += 0.24;
+            $(".total-cost").attr("id", control_total_value.toPrecision(3));
+            $(".total-cost").html("Total: -$" + control_total_value.toPrecision(3) + " per hour");
         }
     })
     
     $("#arrow-down").on("click", function() {
         var temp_value = Number($(".temp-degree").attr("id"));
+        var control_total_value = Number( $(".total-cost").attr("id"));
         if (temp_value > 60) {
             //change temp display
             temp_value--;
@@ -60,6 +73,10 @@ $(document).ready(function() {
             cost -= 0.24;
             $(".temp-item-cost").attr("id", cost.toLocaleString());
             $(".temp-item-cost").html("-$" + cost.toLocaleString());
+            
+            control_total_value -= 0.24;
+            $(".total-cost").attr("id", control_total_value.toPrecision(3));
+            $(".total-cost").html("Total: -$" + control_total_value.toPrecision(3) + " per hour");
         }
     })
     
