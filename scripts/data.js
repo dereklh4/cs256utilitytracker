1676b8e4cea3475efb4cf0735f3e1d8063e73742
Derek Hancock <derekhancock44@gmail.com>
2016-03-30 00:16:44 -0600

    faq page. More graphs on data page

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,9 @@
+var type = "all";
+var period = "month";
+
 $(document).ready(function() {
 
-    $(".category-button").on("click", function() {
+    $(".button").on("click", function() {
         if ($(this).hasClass("selected")) {
             //do nothing
         }
@@ -8,18 +11,57 @@ $(document).ready(function() {
             $(this).parent().find(".selected").toggleClass("selected");
             $(this).toggleClass("selected");
             
+            //update variable
+            var id = $(this).attr("id");
+            if (id == "all" || id == "electric" || id=="gas" || id=="water") {
+                type = id;
+            }
+            else {
+                period = id;
+            }
+            
             //change the image
-            if ($(this).attr("id") == "all") {
-                $("#graph-img").attr("src", "../../content/graph_all.png");
+            if (period == "month") {
+                if (type == "all") {
+                    $("#graph-img").attr("src", "../../content/graph_all.png");
+                }
+                else if (type == "electric") {
+                    $("#graph-img").attr("src", "../../content/graph_electric.png");
+                }
+                else if (type == "gas") {
+                    $("#graph-img").attr("src", "../../content/graph_gas.png");
+                }
+                else if (type == "water") {
+                    $("#graph-img").attr("src", "../../content/graph_water.png");
+                }
             }
-            else if ($(this).attr("id") == "electric") {
-                $("#graph-img").attr("src", "../../content/graph_electric.png");
+            else if (period == "day") {
+                if (type == "all") {
+                    $("#graph-img").attr("src", "../../content/graph_all_day.png");
+                }
+                else if (type == "electric") {
+                    $("#graph-img").attr("src", "../../content/graph_electric_day.png");
+                }
+                else if (type == "gas") {
+                    $("#graph-img").attr("src", "../../content/graph_gas_day.png");
+                }
+                else if (type == "water") {
+                    $("#graph-img").attr("src", "../../content/graph_water_day.png");
+                }
             }
-            else if ($(this).attr("id") == "gas") {
-                $("#graph-img").attr("src", "../../content/graph_gas.png");
-            }
-            else if ($(this).attr("id") == "water") {
-                $("#graph-img").attr("src", "../../content/graph_water.png");
+            else if (period == "year") { //change these to images of the year, not month.
+                if (type == "all") {
+                    $("#graph-img").attr("src", "../../content/graph_all.png");
+                }
+                else if (type == "electric") {
+                    $("#graph-img").attr("src", "../../content/graph_electric.png");
+                }
+                else if (type == "gas") {
+                    $("#graph-img").attr("src", "../../content/graph_gas.png");
+                }
+                else if (type == "water") {
+                    $("#graph-img").attr("src", "../../content/graph_water.png");
+                }
             }
         }
 
--- scripts/faq.js
+++ scripts/faq.js
@@ -1,1 +1,14 @@
-console.log("inside faq");
+$(document).ready(function() {
+
+    //click events to expand items
+    $(".caret").add(".item-title").add(".item-cost").click(function() {
+        var parent = $(this).parent();
+        $(parent).find("span").toggleClass("right");
+        $(parent).find("span").toggleClass("down");
+
+        var visibility = $(parent).find(".item-details").toggle();
+        
+        $(parent).find(".item-details").toggleClass("padding-bottom-5px");
+    })
+    
+});
