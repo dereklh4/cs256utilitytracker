96e3a03ef787225a622f01a0eb8b287c15799fee
Derek Hancock <derekhancock44@gmail.com>
2016-03-29 18:51:24 -0600

    Added temp changing to control page

--- scripts/control.js
+++ scripts/control.js
@@ -22,13 +22,45 @@ $(document).ready(function() {
     });
 
 
-    //click events
+    //click events to expand items
     $(".caret").add(".item-title").add(".item-cost").click(function() {
         var parent = $(this).parent();
         $(parent).find("span").toggleClass("right");
         $(parent).find("span").toggleClass("down");
 
         var visibility = $(parent).find(".item-details").toggle();
+    })
+    
+    $("#arrow-up").on("click", function() {
+        var temp_value = Number($(".temp-degree").attr("id"));
+        if (temp_value < 80) {
+            //change temp display
+            temp_value++;
+            $(".temp-degree").attr("id", temp_value.toLocaleString());
+            $(".temp-degree").html(temp_value.toLocaleString() + "&#176;");
+
+            //change cost
+            var cost = Number($(".temp-item-cost").attr("id"));
+            cost += 0.24;
+            $(".temp-item-cost").attr("id", cost.toLocaleString());
+            $(".temp-item-cost").html("-$" + cost.toLocaleString());
+        }
+    })
+    
+    $("#arrow-down").on("click", function() {
+        var temp_value = Number($(".temp-degree").attr("id"));
+        if (temp_value > 60) {
+            //change temp display
+            temp_value--;
+            $(".temp-degree").attr("id", temp_value.toLocaleString());
+            $(".temp-degree").html(temp_value.toLocaleString() + "&#176;");
+
+            //change cost
+            var cost = Number($(".temp-item-cost").attr("id"));
+            cost -= 0.24;
+            $(".temp-item-cost").attr("id", cost.toLocaleString());
+            $(".temp-item-cost").html("-$" + cost.toLocaleString());
+        }
     })
 
 });
