35d81417b6649d7faa6e595deed0502b0bff174a
Derek Hancock <derekhancock44@gmail.com>
2016-03-30 10:15:41 -0600

    faq page adjustment

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
 
     //click events to expand items
-    $(".caret").add(".item-title").add(".item-cost").click(function() {
-        var parent = $(this).parent();
-        $(parent).find("span").toggleClass("right");
-        $(parent).find("span").toggleClass("down");
+    // $(".caret").add(".item-title").add(".item-cost").click(function() {
+    //     var parent = $(this).parent();
+    //     $(parent).find("span").toggleClass("right");
+    //     $(parent).find("span").toggleClass("down");
 
-        var visibility = $(parent).find(".item-details").toggle();
+    //     var visibility = $(parent).find(".item-details").toggle();
         
-        $(parent).find(".item-details").toggleClass("padding-bottom-5px");
+    //     $(parent).find(".item-details").toggleClass("padding-bottom-5px");
+    // })
+    
+    $(".item").click(function() {
+        $(this).find("span").toggleClass("right");
+        $(this).find("span").toggleClass("down");
+
+        var visibility = $(this).find(".item-details").toggle();
+        
+        $(this).find(".item-details").toggleClass("padding-bottom-5px");
     })
     
 });
