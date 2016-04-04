2802ac9df96a47ba02ed9bf446c14de3c4cc5cee
Derek Hancock <derekhancock44@gmail.com>
2016-04-04 17:52:53 -0600

    add popup capability

---
+++ scripts/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {   
+    //uncomment to add popup
+    // var popup = sessionStorage.getItem("popup");
+    
+    // if (!popup) {
+    //     $("#trigger_popup")[0].click();
+    //     sessionStorage.setItem("popup", "false")
+    // }
+    
+});
